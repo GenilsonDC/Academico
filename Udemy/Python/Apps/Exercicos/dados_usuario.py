7af567e105995eba91971bcc29142ea0b4193dce
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-05-06 01:01:57 -0300

    🐍 Python Exercicio strings
    Fiz desafio de string onde recebe uma string e é feitas alguma analises como tamanho, inverso, espaços, primeirp e ultimo caracter etc

---
+++ Udemy/Python/Apps/Exercicos/dados_usuario.py
@@ -0,0 +1,15 @@
+# aqui o objetivo é receber alguns dados do usuario e fatiar, inverter, procurar, medir
+nome= input('\nDigite seu nome: ')
+idade= input('\nDigite sua idade: ')
+if nome == '' or idade == '':
+    input('\n* * * ALERTA!!! campos vazios * * * ')
+else:
+    print(f'\n Seu nome é: {nome}')
+    print(f'\n Seu nome invertido é: {nome[::-1]}')
+    if ' ' in nome:
+        print('\nO seu nome tem espaços')
+    else:
+        print('\nseu nome não tem espaços')
+print(f'\n o nome {nome} tem {len(nome)} letras')# Também funcionou (f'...', len(nome),'...')
+print(f'\n A primeira letra do nome é: {nome[0]}')
+print(f'\n A ultima letra do nome é: {nome[-1]}')
