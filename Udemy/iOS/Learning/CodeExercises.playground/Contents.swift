58a8f4f976f540d17cf1bfce47328054d8135434
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-07-05 11:26:52 -0300

    🟠 Swift - DadosSort 🎲
    Criando evento com click do botão, os "dados" são mostrados de maneira randômica

---
+++ Udemy/iOS/Learning/CodeExercises.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+
+    let numbers = [45, 73, 195, 53]
+  
+    
+    let computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[3]*numbers[0]]
+
+   
+    
+
+    
+    print(computedNumbers)
+  
+
+
+
