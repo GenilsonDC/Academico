5540f73399f67c49be49c23f266a0a1fe69cace5
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-07-06 10:29:06 -0300

    🟠 Swift - Password generator Challenge
    Desafio de gerar uma senha de 6 caracteres aleatórios vindo do array de 26 componentes, no caso forem letras do alfabeto.

--- Udemy/iOS/Learning/CodeExercises.playground/Contents.swift
+++ Udemy/iOS/Learning/CodeExercises.playground/Contents.swift
@@ -1,15 +1,17 @@
-
-
-    let numbers = [45, 73, 195, 53]
-  
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
-    let computedNumbers = [numbers[0]*numbers[1], numbers[1]*numbers[2], numbers[2]*numbers[3], numbers[3]*numbers[0]]
+var pass1 = alphabet.randomElement()!
+var pass2 = alphabet.randomElement()!
+var pass3 = alphabet.randomElement()!
+var pass4 = alphabet.randomElement()!
+var pass5 = alphabet.randomElement()!
+var pass6 = alphabet.randomElement()!
 
-   
     
+let password = pass1 + pass2 + pass3 + pass4 + pass5 + pass6
 
     
-    print(computedNumbers)
+    print(password)
   
 
 
