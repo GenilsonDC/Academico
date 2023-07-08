7c377b150b4be6cae6888236fc8fb1c01b068fc7
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-07-07 23:30:44 -0300

    🟠 Swift - Desafio Magic Ball ✨
    Desafio de criar App de "respostas magicas"
    Eu criei do zero e fazendo um modelo original.

---
+++ Udemy/iOS/Desafios/BolaRespostaMagica/BolaRespostaMagica/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BolaRespostaMagica
+//
+//  Created by Genilson do Carmo Silva on 07/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageFundo: UIImageView!
+    
+    @IBAction func btnRespostas(_ sender: UIButton) {
+        let randomRespostas = [#imageLiteral(resourceName: "yes"), #imageLiteral(resourceName: "no"), #imageLiteral(resourceName: "é sim"), #imageLiteral(resourceName: "later"), #imageLiteral(resourceName: "misterio"), #imageLiteral(resourceName: "irineu")]
+        
+        imageFundo.image = randomRespostas.randomElement()
+    }
+}
+
+
+
+
