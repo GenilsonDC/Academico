ff55801905241f81ccc14f79765039adaef0fd2a
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-07-15 01:04:44 -0300

    🟠 Swift - Desafio/Charleng - Layout da Calculadora
    Desafio de deixar o layout da calculadora responsivo as diferentes orientações de dimensões do dispositivo.
     Feito ✅
    
    Aproveitei e customizei, add imagens e coloquei interação com alguns botes.

---
+++ Udemy/iOS/Desafios- Challenges/Calculator-Layout/Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Angela Yu on 01/07/2019 Modified by 👑 Genilson do Carmo on 07/15/2023.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DisplayView: UILabel!
+    
+    @IBAction func btnDivisaoView(_ sender: UIButton) {
+        DisplayView.text = "÷"
+        
+    }
+    @IBAction func btnMultiplicarView(_ sender: UIButton) {
+        DisplayView.text = "x"
+    }
+    
+    @IBAction func btnSubtrairView(_ sender: UIButton) {
+        DisplayView.text = "-"
+    }
+    
+    @IBAction func btnAdicionarView(_ sender: UIButton) {
+        DisplayView.text = "+"
+    }
+    
+    @IBAction func btnPercentView(_ sender: UIButton) {
+        DisplayView.text = "%"
+    }
+    @IBAction func btnMoreLessView(_ sender: UIButton) {
+        DisplayView.text = "+/-"
+    }
+    
+    @IBAction func btnClearView(_ sender: UIButton) {
+        DisplayView.text = ""
+    }
+    
+}
+
