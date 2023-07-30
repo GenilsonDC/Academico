f57c4abcfb9357427a32fd1cafe62351c61d2ec4
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-07-30 11:41:00 -0300

    ⚛️ React, Native e MongoDB - Route test
    Teste de rotas finalizado.
    Route test finished.
    
    Testes via Postman e Insominia

--- Udemy/ReactMongo/Backend/src/Controller/taskController.js
+++ Udemy/ReactMongo/Backend/src/Controller/taskController.js
@@ -1,5 +1,16 @@
 const { response } = require("express");
 const taskModel = require("../Model/taskModel");
+const currentDate = new Date();
+const {
+  startOfDay,
+  endOfDay,
+  startOfWeek,
+  endOfWeek,
+  startOfYear,
+  endOfYear,
+  startOfMonth,
+  endOfMonth,
+} = require("date-fns");
 
 class taskController {
   async create(req, res) {
@@ -7,12 +18,12 @@ class taskController {
     await task
       .save()
       .then((response) => {
-        return res.status(200).json(response);
+        return res.status(200).json({ response: "✅ Tarefa criada" });
       })
       .catch((error) => {
         return res
           .status(500)
-          .json("⚠️ Não foi possível criar tarefa: " + error);
+          .json({ error: "⚠️ Não foi possível criar tarefa! " });
       });
   }
   async updated(req, res) {
@@ -20,12 +31,12 @@ class taskController {
       .findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
 
       .then((response) => {
-        return res.status(200).json(response);
+        return res.status(200).json({ response: "✅ Tarefa atualizada" });
       })
       .catch((error) => {
         return res
           .status(500)
-          .json("⚠️ Não foi possível atualizar a tarefa: " + error);
+          .json({ error: "⚠️ Não foi possível atualizar a tarefa! " });
       });
   }
   async all(req, res) {
@@ -38,7 +49,147 @@ class taskController {
       .catch((error) => {
         return res
           .status(500)
-          .json("⚠️ Não foi possível mostrar todas as tarefa: " + error);
+          .json({ error: "⚠️ Não foi possível mostrar todas as tarefa! " });
+      });
+  }
+  async show(req, res) {
+    await taskModel
+      .findById(req.params.id)
+
+      .then((response) => {
+        if (response) return res.status(200).json(response);
+        else
+          return res
+            .status(404)
+            .json({ error: "⚠️ Não foi possível encontrar a tarefa! " });
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possível mostrar a tarefa! " });
+      });
+  }
+  async delete(req, res) {
+    await taskModel
+      .deleteOne({ _id: req.params.id })
+
+      .then((response) => {
+        if (response)
+          return res.status(200).json({ response: "✅ Tarefa deletada " });
+        else
+          return res
+            .status(404)
+            .json({ error: "⚠️ Não foi possível encontrar a tarefa! " });
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possível excluir a tarefa! " });
+      });
+  }
+  async done(req, res) {
+    await taskModel
+      .findByIdAndUpdate(
+        { _id: req.params.id },
+        { done: req.params.done },
+        { new: true }
+      )
+      .then((response) => {
+        if (response)
+          return res.status(200).json({ response: "✅ Tarefa atualizada " });
+        else
+          return res
+            .status(404)
+            .json({ error: "⚠️ Não foi possível encontrar a tarefa! " });
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possível atualizar a tarefa! " });
+      });
+  }
+  async late(req, res) {
+    await taskModel
+      .find({
+        when: { $lt: currentDate },
+        mac_address: { $in: req.body.mac_address },
+      })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possível encontrar a tarefa! " });
+      });
+  }
+  async today(req, res) {
+    await taskModel
+      .find({
+        mac_address: { $in: req.body.mac_address },
+        when: { $gte: startOfDay(currentDate), $lte: endOfDay(currentDate) },
+      })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possivel encontrar a tarefa!" });
+      });
+  }
+  async week(req, res) {
+    await taskModel
+      .find({
+        mac_address: { $in: req.body.mac_address },
+        when: { $gte: startOfWeek(currentDate), $lte: endOfWeek(currentDate) },
+      })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possivel encontrar a tarefa!" });
+      });
+  }
+  async month(req, res) {
+    await taskModel
+      .find({
+        mac_address: { $in: req.body.mac_address },
+        when: {
+          $gte: startOfMonth(currentDate),
+          $lte: endOfMonth(currentDate),
+        },
+      })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possivel encontrar a tarefa!" });
+      });
+  }
+
+  async year(req, res) {
+    await taskModel
+      .find({
+        mac_address: { $in: req.body.mac_address },
+        when: { $gte: startOfYear(currentDate), $lt: endOfYear(currentDate) },
+      })
+      .sort("when")
+      .then((response) => {
+        return res.status(200).json(response);
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ error: "⚠️ Não foi possivel encontrar a tarefa!" });
       });
   }
 }
--- Udemy/ReactMongo/Backend/src/Routes/taskRoutes.js
+++ Udemy/ReactMongo/Backend/src/Routes/taskRoutes.js
@@ -6,6 +6,15 @@ const macAdressValidation = require("../Middlewares/macAdressValidation");
 
 router.post("/", taskValidation, taskController.create);
 router.put("/:id", taskValidation, taskController.updated);
+router.get("/:id", taskController.show);
+router.delete("/:id", taskController.delete);
+router.put("/:id/:done", taskController.done);
+
 router.get("/filter/all", macAdressValidation, taskController.all);
-// router.delete("", taskController.delete);
+router.get("/filter/late", macAdressValidation, taskController.late);
+router.get("/filter/today", macAdressValidation, taskController.today);
+router.get("/filter/week", macAdressValidation, taskController.week);
+router.get("/filter/month", macAdressValidation, taskController.month);
+router.get("/filter/year", macAdressValidation, taskController.year);
+
 module.exports = router;
