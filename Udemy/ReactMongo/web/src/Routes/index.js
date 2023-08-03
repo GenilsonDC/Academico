70c386f59f1c1a71d03c8bcc323e05b64d0b49ad
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-08-03 14:07:27 -0300

    updates

---
+++ Udemy/ReactMongo/web/src/Routes/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "../View/Home";
+import Task from "../View/Task";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/task" exact component={Task} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
