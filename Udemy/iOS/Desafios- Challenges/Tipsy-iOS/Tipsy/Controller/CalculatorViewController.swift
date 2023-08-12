f540a93fd9b4f2f9e2320336b027dfed3435cb00
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-08-11 23:09:47 -0300

    🧡 Swift

---
+++ Udemy/iOS/Desafios- Challenges/Tipsy-iOS/Tipsy/Controller/CalculatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019 and modified by Genilson do Carmo on 09/08/2023
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPercentButton: UIButton!
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var twetyPercentButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+   
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Udemy/iOS/Desafios- Challenges/Tipsy-iOS/Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Genilson do Carmo Silva on 10/08/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+ 
+
+}
