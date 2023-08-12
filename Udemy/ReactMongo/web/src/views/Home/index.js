00411f969b71b57f0609f347157c2072ac8af1f2
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-08-12 08:55:22 -0300

    ⚛️ React - Native

--- Udemy/ReactMongo/web/src/views/Home/index.js
+++ Udemy/ReactMongo/web/src/views/Home/index.js
@@ -1,81 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import {Link, Redirect} from 'react-router-dom';
-import * as S from './styles';
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import * as S from "./styles";
 
-import api from '../../services/api';
-import isConnected from '../../utils/isConnected';
+import api from "../../services/api";
+import isConnected from "../../utils/isConnected";
 
 //NOSSOS COMPONENTES
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import FilterCard from '../../components/FilterCard';
-import TaskCard from '../../components/TaskCard';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import FilterCard from "../../components/FilterCard";
+import TaskCard from "../../components/TaskCard";
 
 function Home() {
-  const [filterActived, setFilterActived] = useState('all');
+  const [filterActived, setFilterActived] = useState("all");
   const [tasks, setTasks] = useState([]);
   const [redirect, setRedirect] = useState(false);
 
-
-  async function loadTasks(){
-    await api.get(`/task/filter/${filterActived}/${isConnected}`)
-    .then(response => {
-      setTasks(response.data)
-    })
+  async function loadTasks() {
+    await api
+      .get(`/task/filter/${filterActived}/${isConnected}`)
+      .then((response) => {
+        setTasks(response.data);
+      });
   }
 
-  function Notification(){
-    setFilterActived('late');
+  function Notification() {
+    setFilterActived("late");
   }
 
   useEffect(() => {
     loadTasks();
 
-    if(!isConnected)
-      setRedirect(true); 
-
-  }, [filterActived, loadTasks])
+    if (!isConnected) setRedirect(true);
+  }, [filterActived, loadTasks]);
 
   return (
     <S.Container>
-      { redirect && <Redirect to="/qrcode"/> }
-      <Header clickNotification={Notification}/>
-      
+      {redirect && <Redirect to="/qrcode" />}
+      <Header clickNotification={Notification} />
+
       <S.FilterArea>
-        <button type="button"        onClick={() => setFilterActived("all")}>
-          <FilterCard title="Todos"  actived={filterActived == 'all'}   />
+        <button type="button" onClick={() => setFilterActived("all")}>
+          <FilterCard title="Todos" actived={filterActived === "all"} />
         </button>
-        <button type="button"        onClick={() => setFilterActived("today")}>
-          <FilterCard title="Hoje"   actived={filterActived == 'today'} />
+        <button type="button" onClick={() => setFilterActived("today")}>
+          <FilterCard title="Hoje" actived={filterActived === "today"} />
         </button>
-        <button type="button"        onClick={() => setFilterActived("week")}>
-          <FilterCard title="Semana" actived={filterActived == 'week'}  />
+        <button type="button" onClick={() => setFilterActived("week")}>
+          <FilterCard title="Semana" actived={filterActived === "week"} />
         </button>
-        <button type="button"        onClick={() => setFilterActived("month")}>
-          <FilterCard title="Mês" actived={filterActived == 'month'}  />
+        <button type="button" onClick={() => setFilterActived("month")}>
+          <FilterCard title="Mês" actived={filterActived === "month"} />
         </button>
-        <button type="button"     onClick={() => setFilterActived("year")}>
-          <FilterCard title="Ano" actived={filterActived == 'year'}  />
-        </button>        
+        <button type="button" onClick={() => setFilterActived("year")}>
+          <FilterCard title="Ano" actived={filterActived === "year"} />
+        </button>
       </S.FilterArea>
 
       <S.Title>
-        <h3>{filterActived == 'late' ? 'TAREFAS ATRASADAS'  : 'TAREFAS'}</h3>
+        <h3>{filterActived == "late" ? "TAREFAS ATRASADAS" : "TAREFAS"}</h3>
       </S.Title>
 
       <S.Content>
-        {
-          tasks.map(t => (
+        {tasks.map((t) => (
           <Link to={`/task/${t._id}`}>
-            <TaskCard type={t.type} title={t.title} when={t.when} done={t.done} />    
+            <TaskCard
+              type={t.type}
+              title={t.title}
+              when={t.when}
+              done={t.done}
+            />
           </Link>
-          ))  
-        }
+        ))}
       </S.Content>
 
-      <Footer/>
+      <Footer />
     </S.Container>
-  )
+  );
 }
 
 export default Home;
