19c4ca9e84d711072b99a37b8069bff9ef7263a3
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-08-23 05:29:53 -0300

    Editado no Windows - Back-end NG

--- Udemy/ReactMongo/ReactNative/RoyalApp/src/components/DateTimeInput/index.ios.js
+++ Udemy/ReactMongo/ReactNative/RoyalApp/src/components/DateTimeInput/index.ios.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
-import { TouchableOpacity, Image, View, TextInput } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  TouchableOpacity,
+  Image,
+  View,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { format } from "date-fns";
+import { format, setHours, setMinutes } from "date-fns";
 import styles from "./styles";
 import iconCalendar from "../../assets/calendar.png";
 import iconClock from "../../assets/clock.png";
 
-export default function DateTimeInput({ type, value, onChange }) {
+export default function ({ type, value, onChange }) {
   const [isPickerVisible, setPickerVisible] = useState(false);
+  const scrollViewRef = useRef(null);
 
   const showPicker = () => {
     setPickerVisible(true);
@@ -15,42 +22,51 @@ export default function DateTimeInput({ type, value, onChange }) {
 
   const hidePicker = () => {
     setPickerVisible(false);
+    scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: true });
   };
 
   const handleConfirm = (selectedValue) => {
-    hidePicker();
-    onChange(selectedValue);
+    if (type === "date" && selectedValue < new Date()) {
+      if (
+        selectedValue.toDateString() === new Date().toDateString() &&
+        selectedValue.getHours() >= new Date().getHours()
+      ) {
+        hidePicker();
+        onChange(selectedValue);
+      } else {
+        scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: true });
+      }
+    } else {
+      hidePicker();
+      onChange(selectedValue);
+    }
   };
 
   const inputValue =
     type === "date" ? format(value, "dd/MM/yyyy") : format(value, "HH:mm");
 
   return (
-    <View style={styles.inputContainer}>
-      <TouchableOpacity style={styles.input} onPress={showPicker}>
-        <TextInput
-          placeholder={
-            type === "date"
-              ? "Clique para definir a data..."
-              : "Clique para definir a hora..."
-          }
-          editable={false}
-          value={inputValue}
-        />
-        <Image
-          style={styles.iconTextInput}
-          source={type === "date" ? iconCalendar : iconClock}
-        />
-      </TouchableOpacity>
-      {isPickerVisible && (
-        <DateTimePickerModal
-          isVisible={isPickerVisible}
-          mode={type === "date" ? "date" : "time"}
-          date={value}
-          onConfirm={handleConfirm}
-          onCancel={hidePicker}
-        />
-      )}
-    </View>
+    <ScrollView ref={scrollViewRef} style={styles.scrollView}>
+      <View style={styles.inputContainer}>
+        <TouchableOpacity style={styles.inputTouchble} onPress={showPicker}>
+          <TextInput style={styles.input} editable={false} value={inputValue} />
+          <Image
+            style={styles.iconTextInput}
+            source={type === "date" ? iconCalendar : iconClock}
+          />
+        </TouchableOpacity>
+        {isPickerVisible && (
+          <DateTimePickerModal
+            isVisible={isPickerVisible}
+            mode={type === "date" ? "date" : "time"}
+            date={value}
+            onConfirm={handleConfirm}
+            onCancel={hidePicker}
+            minimumDate={type === "time" ? undefined : new Date()} // Impede datas passadas apenas para seleção de data
+            minuteInterval={10} // Intervalo de minutos
+          />
+        )}
+      </View>
+    </ScrollView>
   );
 }
--- Udemy/ReactMongo/ReactNative/RoyalApp/src/components/DateTimeInput/styles.js
+++ Udemy/ReactMongo/ReactNative/RoyalApp/src/components/DateTimeInput/styles.js
@@ -1,29 +1,31 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    alignItems: "center",
+  inputTouchble: {
+    width: "100%",
+    height: 70,
+    padding: 10,
+    alignContent: "center",
   },
-
   input: {
-    color: "#FF4500",
-    fontSize: 18,
+    color: "#000080",
+    textAlign: "left",
+    fontSize: 19,
     fontWeight: "bold",
     width: "100%",
+    height: 40,
     borderBottomWidth: 2,
-    borderRadius: 25,
+    borderRadius: 15,
     borderColor: "#708090",
-    padding: 22,
-    margin: 20,
   },
   iconTextInput: {
-    position: "absolute",
-    resizeMode: "center",
-
-    bottom: 15,
-    right: 25,
-    width: 25,
-    height: 25,
+    resizeMode: "contain",
+    bottom: 38,
+    width: "180%",
+    height: 32,
+  },
+  scrollView: {
+    width: "100%",
   },
 });
 export default styles;
