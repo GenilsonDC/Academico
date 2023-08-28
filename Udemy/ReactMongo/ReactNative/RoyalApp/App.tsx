b71ae33c50327eed926b0ed0dafea17ab89bd41b
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-08-27 21:12:56 -0300

    ND

--- Udemy/ReactMongo/ReactNative/RoyalApp/App.tsx
+++ Udemy/ReactMongo/ReactNative/RoyalApp/App.tsx
@@ -25,6 +25,6 @@ import Home from "./src/View/Home";
 import Task from "./src/View/Task";
 
 export default function App() {
-  // return <Task />;
-  return <Home />;
+  return <Task />;
+  // return <Home />;
 }
