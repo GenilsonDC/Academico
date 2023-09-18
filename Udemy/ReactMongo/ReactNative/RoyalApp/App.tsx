b1209fe02870f7e77a3979d921353001f63befe3
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-09-18 17:03:23 -0300

    Update

--- Udemy/ReactMongo/ReactNative/RoyalApp/App.tsx
+++ Udemy/ReactMongo/ReactNative/RoyalApp/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
 
 export default App;
 
-//************MANUAL FUNCIONAL*********/
+//************ MANUAL FUNCIONAL *********/
 // export default function App() {
 //   return <Task />;
 // }
