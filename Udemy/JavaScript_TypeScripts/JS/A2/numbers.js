87c8143e6ad45b5e8bb35fb32f47e3a4a1671193
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-03-04 16:37:29 -0300

    JavaScript

---
+++ Udemy/JavaScript_TypeScripts/JS/A2/numbers.js
@@ -0,0 +1,9 @@
+let num1 = 129
+let num2 = 23.985454884
+// console.log(num1.toString(2))
+// console.log(num2.toFixed(2))
+console.log(Number.isNaN(num2))
+// let i = num1 * 'B'
+let i = num1 * '7'
+console.log(i)
+console.log(Number.isNaN(i))
---
+++ Udemy/JavaScript_TypeScripts/JS/exercises/JS/ex3.js
@@ -0,0 +1,13 @@
+const nome = prompt('Digite seu nome completo: ');
+
+// Saidas no DEVTOOLS
+// document.body.innerHTML += `<b>Seu nome é:</b> ${nome} <br\>`;
+// document.body.innerHTML += `<b>Seu nome tem:</b> ${nome.length} letras <br\>`;
+// document.body.innerHTML += `<b>A segunda letra do seu nome é:</b> ${nome[1]} <br\>`;
+// document.body.innerHTML += `<b>O primeiro indice da leta "a" do seu nome é:</b> ${nome.indexOf("a")}. <br \>`;
+// document.body.innerHTML += `<b>O ultimo indice da leta "a" do seu nome é:</b> ${nome.lastIndexOf("a")}. <br \>`;
+// document.body.innerHTML += `<b>As ultimas 3 letras do seu nome são:</b> ${nome.slice(-3)}. <br \>`;
+// document.body.innerHTML += `<b>As palavras do seu nome são:</b> ${nome.split(' ')}. <br \>`;
+// document.body.innerHTML += `<b>Seu nome em lowercase é:</b> ${nome.toLowerCase()} <br \>`;
+// document.body.innerHTML += `<b>Seu nome em UPERCASE é:</b> ${nome.toUpperCase()} <br \>`;
+
