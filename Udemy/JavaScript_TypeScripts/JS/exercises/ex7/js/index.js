c23c1f0ae6175974cfcee1bc62d7148ceb3cd978
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-03-17 21:25:21 -0300

    atualização exercicio
    clean code

--- Udemy/JavaScript_TypeScripts/JS/exercises/ex7/js/index.js
+++ Udemy/JavaScript_TypeScripts/JS/exercises/ex7/js/index.js
@@ -1,98 +1,110 @@
-function showDate() {
-    const texts = document.querySelector('.text');
+// function showDate() {
+//     const texts = document.querySelector('.texts h1');
 
-    function daysOfWeek(day) {
-        let dayText;
-        switch (day) {
-            case 0:
-                dayText = `Domingo`
-                return dayText;
-            case 1:
-                dayText = `Segunda feira`
-                return dayText;
-            case 2:
-                dayText = `Terça feira`
-                return dayText;
-            case 3:
-                dayText = `Quarta feira`
-                return dayText;
-            case 4:
-                dayText = `Quinta feira`
-                return dayText;
-            case 5:
-                dayText = `Sexta feira`
-                return dayText;
-            case 6:
-                dayText = `Sábado`
-                return dayText;
-        }
-    }
+//     function daysOfWeek(day) {
+//         let dayText;
+//         switch (day) {
+//             case 0:
+//                 dayText = `Domingo`
+//                 return dayText;
+//             case 1:
+//                 dayText = `Segunda feira`
+//                 return dayText;
+//             case 2:
+//                 dayText = `Terça feira`
+//                 return dayText;
+//             case 3:
+//                 dayText = `Quarta feira`
+//                 return dayText;
+//             case 4:
+//                 dayText = `Quinta feira`
+//                 return dayText;
+//             case 5:
+//                 dayText = `Sexta feira`
+//                 return dayText;
+//             case 6:
+//                 dayText = `Sábado`
+//                 return dayText;
+//         }
+//     }
 
-    function monthsOfYear(month) {
-        let monthText;
-        switch (month) {
-            case 0:
-                monthText = `janeiro`
-                return monthText;
-            case 1:
-                monthText = `fevereiro`
-                return monthText;
-            case 2:
-                monthText = `março`
-                return monthText;
-            case 3:
-                monthText = `abril`
-                return monthText;
-            case 4:
-                monthText = `maio`
-                return monthText;
-            case 5:
-                monthText = `junho`
-                return monthText;
-            case 6:
-                monthText = `julio`
-                return monthText;
-            case 7:
-                monthText = `agosto`
-                return monthText;
-            case 8:
-                monthText = `setembro`
-                return monthText;
-            case 9:
-                monthText = `outubro`
-                return monthText;
-            case 10:
-                monthText = `novembro`
-                return monthText;
-            case 11:
-                monthText = `dezembro`
-                return monthText;
-        }
-    }
+//     function monthsOfYear(month) {
+//         let monthText;
+//         switch (month) {
+//             case 0:
+//                 monthText = `janeiro`
+//                 return monthText;
+//             case 1:
+//                 monthText = `fevereiro`
+//                 return monthText;
+//             case 2:
+//                 monthText = `março`
+//                 return monthText;
+//             case 3:
+//                 monthText = `abril`
+//                 return monthText;
+//             case 4:
+//                 monthText = `maio`
+//                 return monthText;
+//             case 5:
+//                 monthText = `junho`
+//                 return monthText;
+//             case 6:
+//                 monthText = `julio`
+//                 return monthText;
+//             case 7:
+//                 monthText = `agosto`
+//                 return monthText;
+//             case 8:
+//                 monthText = `setembro`
+//                 return monthText;
+//             case 9:
+//                 monthText = `outubro`
+//                 return monthText;
+//             case 10:
+//                 monthText = `novembro`
+//                 return monthText;
+//             case 11:
+//                 monthText = `dezembro`
+//                 return monthText;
+//         }
+//     }
 
-    function zeroEsquerda(num) {
-        return num >= 10 ? num : `0${num}`;
-    }
+//     function zeroEsquerda(num) {
+//         return num >= 10 ? num : `0${num}`;
+//     }
 
-    function getDates(dates) {
-        const sec = zeroEsquerda(dates.getSeconds());
-        const min = zeroEsquerda(dates.getMinutes());
-        const hor = zeroEsquerda(dates.getHours());
-
-
-        return `${hor}:${min}:${sec}`;
-    }
-    const dates = new Date();
-    const day = dates.getDay();
-    const month = dates.getMonth();
-    const year = dates.getFullYear();
-    const dayText = daysOfWeek(day);
-    const monthText = monthsOfYear(month);
-    const brazilHors = getDates(dates);
-
-    texts.innerHTML = '';
-    texts.innerHTML = `<p class="timerContainer"> ${dayText}, ${day} de ${monthText} de ${year} - ${brazilHors}</p>`
+//     function getDates(dates) {
+//         const sec = zeroEsquerda(dates.getSeconds());
+//         const min = zeroEsquerda(dates.getMinutes());
+//         const hor = zeroEsquerda(dates.getHours());
 
 
-};
-showDate();
+//         return `${hor}:${min}:${sec}`;
+//     }
+//     const dates = new Date();
+//     const day = dates.getDay();
+//     const month = dates.getMonth();
+//     const year = dates.getFullYear();
+//     const dayText = daysOfWeek(day);
+//     const monthText = monthsOfYear(month);
+//     const brazilHors = getDates(dates);
+
+//     texts.innerHTML = '';
+//     texts.innerHTML = `<p class="timerContainer"> ${dayText}, ${day} de ${monthText} de ${year} - ${brazilHors}</p>`
+
+
+// };
+// showDate();
+
+// ************* FORMA MAIS CURTA **********
+
+const texts = document.querySelector('.texts h1');
+const data = new Date();
+// const opcoes = {
+//     dateStyle: 'full',
+//     timeStyle: 'short'
+// }
+// texts.innerHTML = data.toLocaleString('pt-BR', opcoes);
+
+texts.innerHTML = data.toLocaleString('pt-BR', { dateStyle: 'full', timeStyle: 'short' });
