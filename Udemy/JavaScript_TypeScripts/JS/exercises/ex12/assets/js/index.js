17f39404c380eed66de0b040ee238d88d69a82fe
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-03-24 15:42:31 -0300

    ✅ Fixed - Start, Stop and Reset buttons - Working

--- Udemy/JavaScript_TypeScripts/JS/exercises/ex12/assets/js/index.js
+++ Udemy/JavaScript_TypeScripts/JS/exercises/ex12/assets/js/index.js
@@ -1,42 +1,51 @@
 
-const texts = document.querySelector('.texts h1');
-const timerDisplay = document.querySelector('.timerDisplay')
-const btnStart = document.querySelector('#btnStart')
-const btnPause = document.querySelector('#btnPause')
-const btnReset = document.querySelector('#btnReset')
 
-const timer = setInterval(function () {
-    retornaHora();
-}, 1000);
+function timer() {
 
+    const timerDisplay = document.querySelector('.timerDisplay')
+    const btnStart = document.querySelector('.btnStart')
+    const btnPause = document.querySelector('.btnPause')
+    const btnReset = document.querySelector('.btnReset')
+    let seg = 0;
+    let timer;
 
+    btnStart.addEventListener('click', function (event) {
+        clearInterval(timer);
+        iniciaSegundos();
+        timerDisplay.classList.remove('redcolor');
 
-btnStart.addEventListener('click', function (event) {
-    setInterval(function () {
-        timerDisplay.innerHTML = `${retornaHora()}`
     });
-});
 
-btnPause.addEventListener('click', function (event) {
-    setTimeout(function () {
-        clearInterval(retornaHora());
-        timerDisplay.innerHTML = `<h1 class="redcolor">${retornaHora()}</h1>`
+    btnPause.addEventListener('click', function (event) {
+        clearInterval(timer);
+        timerDisplay.classList.add('redcolor');
     });
-});
 
-btnReset.addEventListener('click', function (event) {
-    timerDisplay.innerHTML = `00:00:00`;
-});
-
-function retornaHora() {
-    const data = new Date();
-    return data.toLocaleTimeString('pt-BR', {
-        timeStyle: 'medium'
+    btnReset.addEventListener('click', function (event) {
+        clearInterval(timer);
+        seg = 0;
+        timerDisplay.innerHTML = `00:00:00`;
+        timerDisplay.classList.remove('redcolor');
     });
-};
+
+    function retornaSegundos(seg) {
+        const data = new Date(seg * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        })
+    };
+
+    function iniciaSegundos() {
+        timer = setInterval(function () {
+            seg++;
+            timerDisplay.innerHTML = `${retornaSegundos(seg)}`
+        }, 1000);
+
+    };
 
 
 
-
+} timer();
 
 
