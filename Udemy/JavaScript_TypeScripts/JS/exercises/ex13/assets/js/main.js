bf99b9262427f2ee520ca8fd23250345bac3b4e0
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-04-10 23:00:26 -0300

    Calculadora HTML

---
+++ Udemy/JavaScript_TypeScripts/JS/exercises/ex13/assets/js/main.js
@@ -0,0 +1,76 @@
+function buildCalc() {
+    return {
+        display: document.querySelector(".display"),
+
+        startCalc() {
+            this.clickButtons();
+            this.pushButtonEnter();
+        },
+
+        pushButtonEnter() {
+            this.display.addEventListener("keyup", event => {
+                if (event.keyCode === 13) {
+                    this.doAccount();
+                }
+            });
+        },
+
+        doAccount() {
+            let account = this.display.value;
+
+            try {
+                account = eval(account);
+
+                if (!account) {
+                    alert(`Operação invalida!`);
+                    return;
+                }
+                this.display.value = account;
+
+            } catch (e) {
+                alert(`Operação invalida!`);
+                return;
+
+            }
+        },
+
+        delOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+
+        clickButtons() {
+            document.addEventListener("click", (event) => {
+                const element = event.target;
+
+                if (element.classList.contains('btnNum')) {
+                    this.showInDisplay(element.innerText);
+                }
+                if (element.classList.contains('btnSimbol')) {
+                    this.showInDisplay(element.innerText);
+                }
+                if (element.classList.contains("btnClear")) {
+                    this.clearDisplay();
+                }
+                if (element.classList.contains("btnDel")) {
+                    this.delOne();
+                }
+                if (element.classList.contains("btnIgual")) {
+                    this.doAccount();
+                }
+
+            });
+        },
+
+        showInDisplay(capValue) {
+            this.display.value += capValue;
+        }
+    }
+};
+
+const calculator = buildCalc();
+calculator.startCalc();
