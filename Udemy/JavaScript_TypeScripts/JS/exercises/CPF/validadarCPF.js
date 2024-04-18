aaf1fd9d80618fa687c5ff475a17260ad2594b3e
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-04-18 09:01:42 -0300

    Validador CPF

---
+++ Udemy/JavaScript_TypeScripts/JS/exercises/CPF/validadarCPF.js
@@ -0,0 +1,46 @@
+// Genilson do Carmo
+// 705.484.450-52
+// 070.987.720-03
+
+
+function ValidaCPF(entradaCPF) {
+    Object.defineProperty(this, "cpfLimpo", {
+        get: function () {
+            return entradaCPF.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidaCPF.prototype.validador = function () {
+    if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.checaSequencia()) return false;
+
+    const cpfSlice = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.geraDigito(cpfSlice);
+    const digito2 = this.geraDigito(cpfSlice + digito1);
+
+    const cpfResultante = cpfSlice + digito1 + digito2;
+    return cpfResultante === this.cpfLimpo;
+}
+
+ValidaCPF.prototype.checaSequencia = function () {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo;
+}
+
+ValidaCPF.prototype.geraDigito = function (cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    let dezDigitos = cpfArray.length + 1;
+    const countRegressivo = cpfArray.reduce((ac, val) => {
+        ac += (dezDigitos * Number(val));//o val é uma String e agora é Number
+        dezDigitos--;
+        return ac;
+
+    }, 0);
+
+    const digito = 11 - (countRegressivo % 11);
+    return digito > 9 ? '0' : String(digito);
+}
+
+const cpf = new ValidaCPF("111.111.111-11");
