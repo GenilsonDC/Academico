a6fd06addbcebfade2bc82532cfb1c51297715a5
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-05-04 21:06:39 -0300

    Update pckg - Ambiente virtual criado

---
+++ Dio/Python/Avancado/pacotes/pacotes.py
@@ -0,0 +1,22 @@
+
+#  _*_Pip_*_ é uma ferramenta de gerenciamento de pacotes
+
+# Ambiente Vitual--> python -m venv .env -->> source .env/bin/activate
+# DESATIVAR Ambiente Vitual--> deactivate
+
+#  _*_Pipenv_*_ é uma ferramenta de gerenciamento de pacotes que combina a gestão de dependeicias com
+#  a criação de ambiente virtual para seus projetos e adiciona/remove pacotes automaticamente do arquiv Pipfile
+# conforme você intala/desinstala pacotes.
+# ***** Comandos
+# pip install pipenv
+# pipenv install numpy
+# pipenv uninstall numpy
+# pipenv graph
+
+#  _*_Poetry_*_ é uma ferramenta de gerenciamento de dependencias que permite declarar as bibliotecas de que seu 
+#projeto depende e gerencia(instala/desinstala/remove) essas lib. Suporta empacotamentoe pub de projetos no Pupl.
+# pip install poetry
+# poetry new myproject
+# cd myproject
+# poetry add numpy
+# poetry remove numpy
